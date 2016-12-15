ac4512d9ae78bc918e930906570a1a3f86fbc70f
NISALI <nisalikularatne@yahoo.com>
2016-12-15 12:41:00 +0000

    Diamond Class added to populate the Map with Diamonds

---
+++ src/MapViewer/Diamond.java
@@ -0,0 +1,48 @@
+package MapViewer;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.image.Image;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.image.Image;
+
+/**
+ * Created by Nisali Kularatne on 17/12/2016.
+ */
+
+
+public class Diamond {
+    public static Image dia;
+    public int xIndex, yIndex;
+    public int size = 16;
+
+    //get the image from resource file
+    public Image loadDiamond(String spritePath){
+        return new Image(spritePath);
+    }
+
+    public void populateDiamond(GraphicsContext g){
+        Diamond diamond = new Diamond();
+        dia=loadDiamond("/Sprites/diamond.gif");
+        diamond.drawDiamond(g, 20, 20);
+        diamond.drawDiamond(g, 4, 28 );
+        diamond.drawDiamond(g, 36, 12 );
+        diamond.drawDiamond(g, 34, 4 );
+        diamond.drawDiamond(g, 19, 28 );
+        diamond.drawDiamond(g, 26, 35 );
+        diamond.drawDiamond(g, 36, 38 );
+        diamond.drawDiamond(g, 28, 27 );
+        diamond.drawDiamond(g, 30, 20 );
+        diamond.drawDiamond(g, 25, 14 );
+        diamond.drawDiamond(g, 21, 4 );
+        diamond.drawDiamond(g, 14, 9 );
+        diamond.drawDiamond(g, 3, 4 );
+        diamond.drawDiamond(g, 14, 20 );
+        diamond.drawDiamond(g, 20, 13 );
+    }
+
+    //draws the diamonds on the map
+    public void drawDiamond(GraphicsContext g, int xIndex, int yIndex){
+
+        g.drawImage(dia, 0, 0, size,size, xIndex*size, yIndex*size, size, size);
+
+    }
+}
